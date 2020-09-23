f43762dfd2b1c8d1859fcaebd36a5d2c9375c2ef
Chaitanyasuma Jain <chaitanyasuma@gmail.com>
2020-09-23 10:45:11 +0530

    Assignment

--- Week_2/handling_files.py
+++ Week_2/handling_files.py
@@ -1,16 +1,6 @@
 #!/usr/bin/env python3
 import csv
 def read_employees(csv_file_location):
-        '''with open(csv_file_location) as file:
-                reader = csv.DictReader(file)
-                csv.register_dialect('empDialect', skipinitialspace=True, strict=True)
-
-        csv.register_dialect('empDialect', skipinitialspace=True, strict=True)
-        employee_file = csv.DictReader(open(csv_file_location), dialect = 'empDialect')
-        employee_list = []
-        for data in employee_file:
-                employee_list.append(data)
-        return employee_list'''
         csv.register_dialect('empDialect', skipinitialspace=True, strict=True)
         employee_file = csv.DictReader(open(csv_file_location), dialect =
             'empDialect')
